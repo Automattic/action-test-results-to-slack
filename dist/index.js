3a3436b183b026e5e6803076ecb7add7900ac81e
anomiex <anomiex@users.noreply.github.com>
2023-10-17 21:34:28 +0000

    Update dependency @actions/core to v1.10.1 (#33650)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6552905692

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 9414:
+/***/ 9525:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
@@ -28,7 +28,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.issue = exports.issueCommand = void 0;
 const os = __importStar(__nccwpck_require__(2037));
-const utils_1 = __nccwpck_require__(8806);
+const utils_1 = __nccwpck_require__(432);
 /**
  * Commands
  *
@@ -100,7 +100,7 @@ function escapeProperty(s) {
 
 /***/ }),
 
-/***/ 4139:
+/***/ 2722:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
@@ -135,12 +135,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getIDToken = exports.getState = exports.saveState = exports.group = exports.endGroup = exports.startGroup = exports.info = exports.notice = exports.warning = exports.error = exports.debug = exports.isDebug = exports.setFailed = exports.setCommandEcho = exports.setOutput = exports.getBooleanInput = exports.getMultilineInput = exports.getInput = exports.addPath = exports.setSecret = exports.exportVariable = exports.ExitCode = void 0;
-const command_1 = __nccwpck_require__(9414);
-const file_command_1 = __nccwpck_require__(5417);
-const utils_1 = __nccwpck_require__(8806);
+const command_1 = __nccwpck_require__(9525);
+const file_command_1 = __nccwpck_require__(8608);
+const utils_1 = __nccwpck_require__(432);
 const os = __importStar(__nccwpck_require__(2037));
 const path = __importStar(__nccwpck_require__(1017));
-const oidc_utils_1 = __nccwpck_require__(4560);
+const oidc_utils_1 = __nccwpck_require__(9629);
 /**
  * The code to exit an action
  */
@@ -425,17 +425,17 @@ exports.getIDToken = getIDToken;
 /**
  * Summary exports
  */
-var summary_1 = __nccwpck_require__(4178);
+var summary_1 = __nccwpck_require__(4116);
 Object.defineProperty(exports, "summary", ({ enumerable: true, get: function () { return summary_1.summary; } }));
 /**
  * @deprecated use core.summary
  */
-var summary_2 = __nccwpck_require__(4178);
+var summary_2 = __nccwpck_require__(4116);
 Object.defineProperty(exports, "markdownSummary", ({ enumerable: true, get: function () { return summary_2.markdownSummary; } }));
 /**
  * Path exports
  */
-var path_utils_1 = __nccwpck_require__(7634);
+var path_utils_1 = __nccwpck_require__(3550);
 Object.defineProperty(exports, "toPosixPath", ({ enumerable: true, get: function () { return path_utils_1.toPosixPath; } }));
 Object.defineProperty(exports, "toWin32Path", ({ enumerable: true, get: function () { return path_utils_1.toWin32Path; } }));
 Object.defineProperty(exports, "toPlatformPath", ({ enumerable: true, get: function () { return path_utils_1.toPlatformPath; } }));
@@ -443,7 +443,7 @@ Object.defineProperty(exports, "toPlatformPath", ({ enumerable: true, get: funct
 
 /***/ }),
 
-/***/ 5417:
+/***/ 8608:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
@@ -475,7 +475,7 @@ exports.prepareKeyValueMessage = exports.issueFileCommand = void 0;
 const fs = __importStar(__nccwpck_require__(7147));
 const os = __importStar(__nccwpck_require__(2037));
 const uuid_1 = __nccwpck_require__(6683);
-const utils_1 = __nccwpck_require__(8806);
+const utils_1 = __nccwpck_require__(432);
 function issueFileCommand(command, message) {
     const filePath = process.env[`GITHUB_${command}`];
     if (!filePath) {
@@ -508,7 +508,7 @@ exports.prepareKeyValueMessage = prepareKeyValueMessage;
 
 /***/ }),
 
-/***/ 4560:
+/***/ 9629:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
@@ -526,7 +526,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.OidcClient = void 0;
 const http_client_1 = __nccwpck_require__(7768);
 const auth_1 = __nccwpck_require__(6662);
-const core_1 = __nccwpck_require__(4139);
+const core_1 = __nccwpck_require__(2722);
 class OidcClient {
     static createHttpClient(allowRetry = true, maxRetry = 10) {
         const requestOptions = {
@@ -558,7 +558,7 @@ class OidcClient {
                 .catch(error => {
                 throw new Error(`Failed to get ID Token. \n 
         Error Code : ${error.statusCode}\n 
-        Error Message: ${error.result.message}`);
+        Error Message: ${error.message}`);
             });
             const id_token = (_a = res.result) === null || _a === void 0 ? void 0 : _a.value;
             if (!id_token) {
@@ -592,7 +592,7 @@ exports.OidcClient = OidcClient;
 
 /***/ }),
 
-/***/ 7634:
+/***/ 3550:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
@@ -657,7 +657,7 @@ exports.toPlatformPath = toPlatformPath;
 
 /***/ }),
 
-/***/ 4178:
+/***/ 4116:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
 "use strict";
@@ -947,7 +947,7 @@ exports.summary = _summary;
 
 /***/ }),
 
-/***/ 8806:
+/***/ 432:
 /***/ ((__unused_webpack_module, exports) => {
 
 "use strict";
@@ -44325,7 +44325,7 @@ module.exports = { isWorkflowFailed, getRunUrl };
 /***/ 5838:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
-const { getInput } = __nccwpck_require__( 4139 );
+const { getInput } = __nccwpck_require__( 2722 );
 const github = __nccwpck_require__( 248 );
 const { WebClient } = __nccwpck_require__( 404 );
 const { debug } = __nccwpck_require__( 2551 );
@@ -44578,7 +44578,7 @@ module.exports = { sendMessage, createMessage };
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const fs = __nccwpck_require__( 7147 );
-const { getInput } = __nccwpck_require__( 4139 );
+const { getInput } = __nccwpck_require__( 2722 );
 const glob = __nccwpck_require__( 4877 );
 const { debug } = __nccwpck_require__( 2551 );
 
@@ -44777,7 +44777,7 @@ module.exports = { getPlaywrightBlocks, getAttachmentPath };
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
 const fs = __nccwpck_require__( 7147 );
-const { getInput } = __nccwpck_require__( 4139 );
+const { getInput } = __nccwpck_require__( 2722 );
 const minimatch = __nccwpck_require__( 6305 );
 const { debug } = __nccwpck_require__( 2551 );
 const extras = __nccwpck_require__( 7437 );
@@ -45326,7 +45326,7 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-const { setFailed, getInput, startGroup, endGroup } = __nccwpck_require__( 4139 );
+const { setFailed, getInput, startGroup, endGroup } = __nccwpck_require__( 2722 );
 const { sendMessage } = __nccwpck_require__( 5838 );
 const { getChannels } = __nccwpck_require__( 9186 );
 
